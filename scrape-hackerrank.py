74b26a3146a0f93c1cdbf766e4e2257583ae5bc9
Victor Suciu <victor@victors-mbp-4.lan>
2020-01-11 22:39:49 -0800

    first commit

---
+++ scrape-hackerrank.py
@@ -0,0 +1,72 @@
+
+import re
+import time
+from selenium import webdriver
+
+URL = 'https://www.hackerrank.com/domains/algorithms'
+
+# driver the entity used by selenius to interact with a browser
+# it should open up its own chrome window
+driver = webdriver.Chrome()
+driver.get(URL)
+
+# The problem at the bottom of the page
+last_problem = "" 
+# the problem that used to be at the bottom of the page
+previous_last_problem = "" 
+# stores whether or not there are any more problems to load
+reached_bottom = False
+
+# regular expression for finding the problem sub-url
+# in the <a> ... </a> division for each problem card
+# Format: "/challenges/problem-name"
+problem_name_regex = re.compile("/challenges/(.*?)(?=\")")
+
+difficulty_regex = re.compile("difficulty (.*?)(?= )")
+
+# Due to hackerrank's "infinite scrolling" feature, most of the problems
+# are not yet inserted into the page's html doc. This while loop
+# continually scrolls down as far as possible and waits for more
+# prblems to load, until it reaches the bottom. This makes all the
+# problem cards accessible in the html doc.
+while not reached_bottom:
+    # gets the parent <div> where all the problem cards are located
+    problem_container = driver.find_element_by_class_name('challenges-list')
+    # converts all html content into one huge, continuous string
+    html_string = problem_container.get_attribute('innerHTML')
+
+    # gets the name of the bottom-most problem
+    last_problem = problem_name_regex.findall(html_string)[-1]
+    # tests if the bottom problem is the same as when it last checked
+    # If so, it has reached the bottom
+    reached_bottom = last_problem == previous_last_problem
+
+    # Tells the webpage to  execute this javascript function 
+    # which scrolls to the bottom of the viewing window
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    previous_last_problem = last_problem
+
+    # Waits three seconds for the page to load more problems.
+    # Crude solution but it works
+    time.sleep(3)
+
+# creates a list of all problem cards
+problem_list = html_string.split('</a>')
+# for some reason, the last entry is always empty. Removes last entry
+problem_list.pop()
+# This stores the final urls
+problem_urls = []
+
+# For every problem card, it finds the problem name
+# and inserts it into a full url and stores it in the list
+for element in problem_list:
+    complete_url = "https://www.hackerrank.com/challenges/" \
+                    + problem_name_regex.findall(element)[0] \
+                    + "/problem"
+    difficulty = difficulty_regex.findall(element)[0]
+    problem_urls.append(complete_url)
+    print(complete_url, "-", difficulty)
+
+
+
