c0af1d02856024fd17fcb3b2d3dfa47b65494492
Victor Suciu <victor@victors-mbp-4.lan>
2020-01-13 00:44:14 -0800

    Added functionality for gathering problem statement/solution pairs. Created hackerrank account for testing.

--- scrape-hackerrank.py
+++ scrape-hackerrank.py
@@ -2,9 +2,14 @@
 import re
 import time
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 URL = 'https://www.hackerrank.com/domains/algorithms'
+count = 0
 
 # driver the entity used by selenius to interact with a browser
 # it should open up its own chrome window
@@ -68,10 +73,99 @@ for element in problem_list:
                     + problem_name_regex.findall(element)[0] \
                     + "/problem"
     difficulty = difficulty_regex.findall(element)[0]
-    problem_urls.append(complete_url)
-    output_file.write(complete_url + "  --  " + difficulty + "\n")
+    problem_urls.append([complete_url, difficulty])
 
+
+wait_for_login = WebDriverWait(driver, 10)
+already_logged_in = False
+
+def parse_problem_statement(statement_html):
+    statement_html = re.sub('(<span)(.+)(?=</span>)(</span>)', '[expression]', statement_html)
+    statement_html = re.sub('(<style)([\s\S]*)(?=</style>)(</style>)', '', statement_html)
+    statement_html = re.sub('((<)(.+?)(?=>))|((</)(.+)(?=>))|>', '', statement_html)
+    return statement_html
+
+def press_esc():
+    action = ActionChains(driver)
+    action.key_down(Keys.ESCAPE)
+    action.key_up(Keys.ESCAPE)
+    action.perform()
+
+def log_in():
+    wait_for_login.until(EC.presence_of_element_located((By.ID, 'Log in')))
+    driver.find_element_by_id('Log in').click()
+    driver.find_element_by_id('input-4').send_keys("HackerrankDataCollection@gmail.com")
+    driver.find_element_by_id('input-5').send_keys("Tech4Good")
+    driver.find_element_by_css_selector('#hr_v2 > div.portal-wrapper > div > div > div > section > div > div > div.tab-list-content > div.login-form.auth-form.theme-m > form > div.form-item.clearfix > button').click()
+    
+
+def get_problem(problem_url):
+    driver.get(problem_url)
+    
+    if(not already_logged_in):
+        log_in()
+    else:
+        press_esc()
+    
+    statement_element = driver.find_element_by_css_selector('#content > div > div > div > div.community-content > div > section > div > div > div > div.full-screen-split.split-wrap.left-pane > section.problem-statement.split > div > div > div > div > div > div.challenge_problem_statement > div > div')
+    statement_string = statement_element.get_attribute('innerHTML')
+    statement_string = parse_problem_statement(statement_string)
+
+    #driver.find_element_by_css_selector('Intentionally Crash Here')
+    
+    action = ActionChains(driver)
+
+    #WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#Leaderboard')))
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#Leaderboard')))
+    if count == 0:
+        time.sleep(1.5)
+    leader_button = driver.find_element_by_css_selector('#Leaderboard')
+
+    action.click(leader_button)
+    action.perform()
+    print("\n\nCLICKED LEADERBOARD\n\n")
+
+    # driver.get(problem_url[0:-7] + "leaderboard")
+    
+    try:   
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#content > div > div > div > div.community-content > div > div.challenge-leaderboard > div > div > div.ui-tabs-wrap > div > section > div:nth-child(1) > div > button > div')))    
+        unlock_button = driver.find_element_by_css_selector('#content > div > div > div > div.community-content > div > div.challenge-leaderboard > div > div > div.ui-tabs-wrap > div > section > div:nth-child(1) > div > button')
+        action.click(unlock_button)
+        action.perform()
+
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#hr_v2 > div.portal-wrapper > div > div > div > section > div > div.ui-dialog-body > div > div > button.btn.hr_primary-btn.hr-dialog-button')))
+        driver.find_element_by_css_selector('#hr_v2 > div.portal-wrapper > div > div > div > section > div > div.ui-dialog-body > div > div > button.btn.hr_primary-btn.hr-dialog-button').click()
+         
+    except:
+        x = "Do Nothing"
+
+    top_link_regex = re.compile('/rest/contests(.*?)(?=\")')
+    top_solution_regex = re.compile('/rest/contests(.*?)(?=\")')
+    try:
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#content > div > div > div > div.community-content > div > div.challenge-leaderboard > div > div > div.ui-tabs-wrap > div > section > div.general-table-wrapper > div > div > div.table-body > div:nth-child(1) > div > div.table-row-column.ellipsis.solutions')))
+        top_solution_link = top_link_regex.findall(driver.find_element_by_css_selector('#content > div > div > div > div.community-content > div > div.challenge-leaderboard > div > div > div.ui-tabs-wrap > div > section > div.general-table-wrapper > div > div > div.table-body > div:nth-child(1) > div > div.table-row-column.ellipsis.solutions').get_attribute('innerHTML'))[0]
+        driver.get("https://www.hackerrank.com/rest/contests" + top_solution_link)
+    except:
+        return[statement_string, 'SOLUTION NOT FOUND']
+    
+
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'body')))
+    top_solution_string = driver.find_element_by_css_selector('body').get_attribute('innerHTML')
+    top_solution_string = re.sub('((<pre)(.+?)(?=>)(>))|(</pre>)', '', top_solution_string)
+    
+    return [statement_string, top_solution_string]
+
+
+for problem_url in problem_urls:
+    data = get_problem(problem_url[0])
+    already_logged_in = True
+    count += 1
+    output_file.write("\n----------\nALGORITHMS." + str(count) + "\n" + problem_url[1] + "\n----------")
+    output_file.write("\nPROBLEM STATEMENT:\n" + data[0] + "\n----------")
+    output_file.write("\nTOP SOLUTION:\n----------\n" + data[1] + "\n----------\n====================")
+    output_file.flush()
 output_file.close()
+
 print("Done")
 
 
